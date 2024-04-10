0136819ad704aa74e2ad142d678cc8fc7eebddce
Radim Pesa <pesaradim@gmail.com>
2024-04-10 18:51:13 +0200

    Add player movement

--- UnityTutorial2D/Assets/Scripts/PlayerController.cs
+++ UnityTutorial2D/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,48 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed;
+    
+    private InputManager inputManager;
+    private PlayerInput playerInput;
+    private InputAction movementAction;
+    private Vector2 moveDirection;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        inputManager = InputManager.instance;
+        playerInput = GetComponent<PlayerInput>();
+
+        movementAction = playerInput.actions["Movement"];
+
+        rb = GetComponent<Rigidbody2D>();
+
+        movementAction.performed += Move;
+
+    }
+
     public void Move(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed)
+        {
+            return;
+        }
+        
+        
+    }
+
+    void Update() // 60x
+    {
+        moveDirection = movementAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate() // 50x
+    {
+        rb.MovePosition(rb.position + moveDirection * (playerSpeed * Time.fixedDeltaTime));
+    }
+
+    void LateUpdate() // 60x
     {
         
     }
