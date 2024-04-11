cf2ba7c437e3b5b47552da74e7688ae31e641806
Radim Pesa <pesaradim@gmail.com>
2024-04-11 16:24:50 +0200

    Add player movement animation and attack animation

--- UnityTutorial2D/Assets/Scripts/PlayerController.cs
+++ UnityTutorial2D/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+public enum PlayerState
+{
+    Move,
+    Attack,
+    Interact
+}
+
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed;
+    public Animator animator;
     
     private InputManager inputManager;
     private PlayerInput playerInput;
     private InputAction movementAction;
     private Vector2 moveDirection;
     private Rigidbody2D rb;
+
+    private PlayerState playerState;
 
     void Awake()
     {
@@ -25,6 +35,7 @@ public class PlayerController : MonoBehaviour
 
         movementAction.performed += Move;
 
+        playerState = PlayerState.Move;
     }
 
     public void Move(InputAction.CallbackContext ctx)
@@ -34,12 +45,39 @@ public class PlayerController : MonoBehaviour
             return;
         }
         
-        
+    }
+
+    public void Attack(InputAction.CallbackContext ctx)
+    {
+        if (!ctx.performed) return;
+
+        StartCoroutine(AttackCo());
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("Attacking", true);
+        playerState = PlayerState.Attack;
+        yield return null;
+        animator.SetBool("Attacking", false);
+        while (animator.GetCurrentAnimatorStateInfo(0).IsName("AttackDown"))
+        {
+            yield return null;
+        }
+        playerState = PlayerState.Move;
     }
 
     void Update() // 60x
     {
-        moveDirection = movementAction.ReadValue<Vector2>();
+        moveDirection = Vector2.zero;
+        
+        if (playerState == PlayerState.Move)
+        {
+            moveDirection = movementAction.ReadValue<Vector2>();
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+            animator.SetFloat("Speed", moveDirection.sqrMagnitude);
+        }
     }
 
     void FixedUpdate() // 50x
